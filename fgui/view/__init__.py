3ca5a3657804511622042b8dc90d6082a503aea6
fmy_xfk <4884059+fmy_xfk@user.noreply.gitee.com>
2025-02-21 11:03:49 +0800

    gui_cmp

--- fgui/view/__init__.py
+++ fgui/view/__init__.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter.ttk import *
 import platform
+from tkinter import messagebox, filedialog
 if platform.system() == "Windows":
     import ctypes
     ctypes.windll.shcore.SetProcessDpiAwareness(1)
